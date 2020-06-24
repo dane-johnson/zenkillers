564180273005d34662b9c8558ffd7e2eb4a253bd
Dane Johnson <daneallenjohnson@protonmail.com>
2020-06-24 15:29:03 -0500

    Take time to switch weapons

--- gamemodes/zenkillers/gamemode/player_ext.lua
+++ gamemodes/zenkillers/gamemode/player_ext.lua
@@ -12,11 +12,15 @@ end
 
 function plymeta:ReceiveWeapons()
    self:StripWeapons()
-   
-   local level = self:GetTowerLevel()
-   if level < TOWER.Size() then
-      self:Give(TOWER.WEAPONS[level])
-   end
-   self:Give("weapon_stunstick")
+
+   local delay = GetConVar("zk_equipdelay")
+   -- Give them new guns after 0.75 seconds (to keep from going to fast)
+   timer.Simple(delay, function()
+                   local level = self:GetTowerLevel()
+                   if level < TOWER.Size() then
+                      local weapon = self:Give(TOWER.WEAPONS[level])
+                   end
+                   self:Give("weapon_stunstick")
+   end)
    -- Prevent default loadout
 end
--- gamemodes/zenkillers/gamemode/shared.lua
+++ gamemodes/zenkillers/gamemode/shared.lua
@@ -4,6 +4,7 @@ GM.Email = "daneallenjohnson@protonmail.com"
 GM.Website = "github.com/dane-johnson"
 
 CreateConVar("zk_towername", "default", FCVAR_NONE, "The name of the file containing the tower weapon names. See workshop for details on adding towers.")
+CreateConVar("zk_equipdelay", 0.75, FCVAR_NEVER_AS_STRING, "How long to wait while switching guns")
 
 GM.NumberOfGuns = 1 -- Default tower, just the stunstick
 
@@ -15,10 +16,12 @@ function GM:Think()
       for _, ply in ipairs(player.GetAll()) do
          if ply:Alive() then
             local weapon = ply:GetActiveWeapon()
-            local ammoType = weapon:GetPrimaryAmmoType()
-            if ammoType ~= -1 then
-               local currentAmmo = ply:GetAmmo()[ammoType] or 0
-               ply:GiveAmmo(9999 - currentAmmo, ammoType, true)
+            if weapon ~= NULL then
+               local ammoType = weapon:GetPrimaryAmmoType()
+               if ammoType ~= -1 then
+                  local currentAmmo = ply:GetAmmo()[ammoType] or 0
+                  ply:GiveAmmo(9999 - currentAmmo, ammoType, true)
+               end
             end
          end
       end
