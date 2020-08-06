0a45125c9f1431843808789f7e201c885f7b9869
Dane Johnson <daneallenjohnson@protonmail.com>
2020-08-06 18:25:18 -0500

    Leader gets a red halo

--- gamemodes/zenkillers/gamemode/cl_init.lua
+++ gamemodes/zenkillers/gamemode/cl_init.lua
@@ -4,3 +4,19 @@ include("cl_hud.lua")
 include("cl_scoreboard.lua")
 include("shared.lua")
 include("player_ext_shd.lua")
+
+local BLOOD_RED = Color(255, 0, 0)
+
+function GM:PreDrawHalos()
+   local leaders = {}
+   local max = 0
+   for _, ply in ipairs(player:GetAll()) do
+      if ply:GetTowerLevel() > max then
+         leaders = {ply}
+         max = ply:GetTowerLevel()
+      elseif ply:GetTowerLevel() == max then
+         table.insert(leaders, ply)
+      end
+   end
+   halo.Add(leaders, BLOOD_RED)
+end
