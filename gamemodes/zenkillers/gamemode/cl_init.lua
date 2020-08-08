486a9810a316ad8a5a8fe26c8012497654a7ca6e
Dane Johnson <daneallenjohnson@protonmail.com>
2020-08-08 12:27:06 -0500

    Not everyone can be the leader at once

--- gamemodes/zenkillers/gamemode/cl_init.lua
+++ gamemodes/zenkillers/gamemode/cl_init.lua
@@ -10,13 +10,21 @@ local BLOOD_RED = Color(255, 0, 0)
 function GM:PreDrawHalos()
    local leaders = {}
    local max = 0
+   local all = true
    for _, ply in ipairs(player:GetAll()) do
       if ply:GetTowerLevel() > max then
          leaders = {ply}
+         if max ~= 0 then
+            all = false
+         end
          max = ply:GetTowerLevel()
       elseif ply:GetTowerLevel() == max then
          table.insert(leaders, ply)
+      else
+         all = false
       end
    end
-   halo.Add(leaders, BLOOD_RED)
+   if not all then
+      halo.Add(leaders, BLOOD_RED)
+   end
 end
